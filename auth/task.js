ad198a2d11a71a62fc9d4f09d5f111756536672b
Danil Krutikov <kdanil.i@yandex.ru>
2024-01-15 19:20:33 +0700

    Second commit

--- auth/task.js
+++ auth/task.js
@@ -2,29 +2,30 @@ const form = document.getElementById('signin__form');
 const welcome = document.getElementById('welcome');
 const signin = document.getElementById('signin');
 const spanId = document.getElementById("user_id")
-form.addEventListener('submit', (e) =>{
+if (localStorage.getItem('user') == null) {
+  form.addEventListener('submit', (e) =>{
   e.preventDefault();
   const formData = new FormData(form);
-  if (localStorage.getItem(formData.get('login')) == null &&
-      formData.get('login') == "demo" &&
-      formData.get('password') == "demo")  {
-    console.log(formData.get('login'))
-    console.log(formData.get('password'))
-    localStorage.setItem("demo", "demo");
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
-    xhr.send(formData);
-    welcome.classList.add("welcome_active");
-    spanId.textContent = formData.get('login')
-    signin.classList.remove("signin_active");
-  } else if (localStorage.getItem(formData.get('login')) == "demo" && formData.get('password') == "demo") {
-        const xhr = new XMLHttpRequest();
-    console.log(formData.get('login'))
-    console.log(formData.get('password'))
-    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
-    xhr.send(formData);
-    welcome.classList.add("welcome_active");
-    spanId.textContent = formData.get('login')
-    signin.classList.remove("signin_active");
-  } else {alert("Неверный логин/пароль")}
-})
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
+  xhr.responseType = 'json';
+  xhr.send(formData);
+  xhr.onload = function() {
+    // console.log(xhr.response.user_id)
+    if (xhr.response.success == true) {
+      welcome.classList.add("welcome_active");
+      spanId.textContent = xhr.response.user_id;
+      signin.classList.remove("signin_active");
+      localStorage.setItem('user', xhr.response.user_id);
+     }
+    else {
+      alert("Неверный логин/пароль");
+    }
+   }
+ })
+}
+else {
+  welcome.classList.add("welcome_active");
+  spanId.textContent = localStorage.getItem('user');
+  signin.classList.remove("signin_active");
+}
