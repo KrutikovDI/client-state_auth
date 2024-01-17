373936c9a59b1473090f42ede8b52c69753014c0
Danil Krutikov <kdanil.i@yandex.ru>
2024-01-17 20:28:37 +0700

    Third commit

--- auth/task.js
+++ auth/task.js
@@ -2,14 +2,21 @@ const form = document.getElementById('signin__form');
 const welcome = document.getElementById('welcome');
 const signin = document.getElementById('signin');
 const spanId = document.getElementById("user_id")
-if (localStorage.getItem('user') == null) {
-  form.addEventListener('submit', (e) =>{
+
+if (localStorage.getItem('user') != null) {
+  welcome.classList.add("welcome_active");
+  spanId.textContent = localStorage.getItem('user');
+  signin.classList.remove("signin_active");
+}
+
+form.addEventListener('submit', (e) =>{
   e.preventDefault();
   const formData = new FormData(form);
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
   xhr.responseType = 'json';
   xhr.send(formData);
+  form.reset()
   xhr.onload = function() {
     // console.log(xhr.response.user_id)
     if (xhr.response.success == true) {
@@ -17,15 +24,10 @@ if (localStorage.getItem('user') == null) {
       spanId.textContent = xhr.response.user_id;
       signin.classList.remove("signin_active");
       localStorage.setItem('user', xhr.response.user_id);
-     }
+    }
     else {
       alert("Неверный логин/пароль");
     }
-   }
- })
-}
-else {
-  welcome.classList.add("welcome_active");
-  spanId.textContent = localStorage.getItem('user');
-  signin.classList.remove("signin_active");
-}
+  }
+})
+
